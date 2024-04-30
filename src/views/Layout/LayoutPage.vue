c16e41fedf4d3b711c17d01a1bc0baff553466ef
Wallacewss2033 <wallace-2033@hotmail.com>
2024-04-30 12:44:42 -0300

    feat: criar layouts

---
+++ src/views/Layout/LayoutPage.vue
@@ -0,0 +1,15 @@
+<!-- src/components/Layout.vue -->
+
+<template>
+  <div>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LayoutPage",
+};
+</script>
