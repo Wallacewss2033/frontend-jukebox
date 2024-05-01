cb41390d792c15fa49c5756dbd80738360f8fe65
Wallacewss2033 <wallace-2033@hotmail.com>
2024-04-30 22:31:38 -0300

    feat: criar card de tasks (modelo)

---
+++ src/components/Card/CardComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card text-start bg-white">
+    <div class="card-header bg-white">
+      <h5 class="card-title">Special title treatment</h5>
+    </div>
+    <div class="card-body text-start">
+      <p class="card-text">
+        With supporting text below as a natural lead-in to additional content.
+      </p>
+    </div>
+    <div class="card-footer text-body-secondary bg-white">
+      <div class="text-start d-flex justify-content-between">
+        <span>
+          <a href="#" class="m-1 text-dark"><i class="fas fa-pen"></i></a>
+          <a href="#" class="m-1 text-dark"><i class="fas fa-trash-alt"></i></a>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import "./styles.css";
+export default {
+  name: "CardComponent",
+};
+</script>
