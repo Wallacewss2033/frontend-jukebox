7a37822d20ae5a6f49008e4f82e933bd528743c6
Wallacewss2033 <wallace-2033@hotmail.com>
2024-04-30 22:32:42 -0300

    add card de task no home

---
+++ src/views/Home/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container" style="background-color: brown;">
+    <div class="box-card row" style="background-color: aqua;">
+      <CardComponent />
+      <CardComponent />
+      <CardComponent />
+      <CardComponent />
+      <CardComponent />
+    </div>
+  </div>
+</template>
+
+<script>
+import CardComponent from "@/components/Card/CardComponent.vue";
+import './styles.css';
+
+export default {
+  name: "HomePage",
+  components: {
+    CardComponent,
+  },
+};
+</script>
