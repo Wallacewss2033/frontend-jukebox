796651546d0ba965f3a5d7333402e25c647e4715
Wallacewss2033 <wallace-2033@hotmail.com>
2024-04-30 22:30:49 -0300

    feat: criar header

---
+++ src/components/Header/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar bg-body-white">
+    <div class="container-fluid">
+      <a class="navbar-brand ms-5" href="#">
+        <img
+          src="../../assets/logo.png"
+          alt="logo"
+        />
+      </a>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
