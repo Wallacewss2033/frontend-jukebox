b9b20e0543e0d85426e401a7142b90b61896d557
Wallacewss2033 <wallace-2033@hotmail.com>
2024-05-02 00:25:12 -0300

    feat: finalizar card de tasks

--- src/components/Card/CardComponent.vue
+++ src/components/Card/CardComponent.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="card text-start bg-white border-white">
     <div class="card-header bg-white">
-      <h5 class="card-title">Special title treatment</h5>
+      <p class="card-title">{{ title }}</p>
     </div>
     <div class="card-body text-start">
       <p class="card-text">
-        With supporting text below as a natural lead-in to additional content.
+        {{ description }}
       </p>
     </div>
     <div class="card-footer text-body-secondary bg-white">
       <div class="text-start d-flex justify-content-between">
         <span>
           <a href="#" class="m-1 text-dark"><i class="fas fa-pen"></i></a>
-          <a href="#" class="m-1 text-dark"><i class="fas fa-trash-alt"></i></a>
+          <a href="#" class="m-1 text-dark" @click="handlerDelete(id)">
+            <i class="fas fa-trash-alt" />
+          </a>
         </span>
       </div>
     </div>
@@ -22,5 +24,18 @@
 import "./styles.css";
 export default {
   name: "CardComponent",
+  props: {
+    id: Number,
+    title: String,
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    handlerDelete(id) {
+      console.log(id);
+    },
+  },
 };
 </script>
--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -1,23 +1,45 @@
 <template>
-  <div class="container" style="background-color: brown;">
-    <div class="box-card row" style="background-color: aqua;">
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
+  <div class="container" style="background-color: brown">
+    <div class="box-card row" style="background-color: aqua">
+      <CardComponent
+        :key="i"
+        v-for="(task, i) in tasks"
+        :id="task.id"
+        :title="task.title"
+        :description="task.description"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import CardComponent from "@/components/Card/CardComponent.vue";
-import './styles.css';
+import "./styles.css";
+import api from "@/services/api";
 
 export default {
   name: "HomePage",
   components: {
     CardComponent,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  mounted() {
+    this.handleGetTasks();
+  },
+  methods: {
+    handleGetTasks() {
+      api
+        .get("/tasks")
+        .then((response) => {
+          this.tasks = response.data.data;
+          console.log(this.tasks);
+        })
+        .catch(() => {});
+    },
   },
 };
 </script>
